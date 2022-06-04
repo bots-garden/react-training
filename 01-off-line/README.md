393e0c0a2311aec622970f3beda07cb1429be357
Philippe Charrière <ph.charriere@gmail.com>
2022-06-04 08:29:34 +0000

    📝 save

---
+++ 01-off-line/README.md
@@ -0,0 +1,5 @@
+
+
+```bash
+wget https://raw.githubusercontent.com/reactjs/reactjs.org/main/static/html/single-file-example.html-O index.html
+```
---
+++ 01-off-line/get-libs.sh
@@ -0,0 +1,4 @@
+
+wget https://unpkg.com/react@18/umd/react.development.js -O js/react.development.js
+wget https://unpkg.com/react-dom@18/umd/react-dom.development.js -O js/react-dom.development.js
+wget https://unpkg.com/@babel/standalone/babel.min.js -O js/babel.min.js
